8dd6474923a7903d749b4bbcbb00e9be567d8e07
khaledA7med <khaledcr22@gmail.com>
2022-11-30 10:22:22 +0200

    some changes

--- src/app/card-view/card-view.component.ts
+++ src/app/card-view/card-view.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { ProductsService } from '../products.service';
 
 
 @Component({
@@ -11,10 +10,15 @@ export class CardViewComponent implements OnInit {
 
   @Input() data:any = {};
   @Output() item = new EventEmitter();
+  addButton:boolean = false;
+  amount:number = 0;
 
-  constructor(private _ProductsService:ProductsService) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
 
+  add(){
+    this.item.emit({item:this.data, quantity:this.amount});
+  }
 }
